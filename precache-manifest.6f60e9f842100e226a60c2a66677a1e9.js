f77651dadfd2564616f7cd83ab1d569ed036e22e
Shreyans Jain <jainshreyans121@gmail.com>
2020-08-24 16:41:20 +0530

    Updates

---
+++ precache-manifest.6f60e9f842100e226a60c2a66677a1e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "948b3647f03f83d251fb9f30c7d15f94",
+    "url": "/React-Practice/index.html"
+  },
+  {
+    "revision": "7217df3ea1dc849cabc4",
+    "url": "/React-Practice/static/css/main.9eb51767.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/React-Practice/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Practice/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7217df3ea1dc849cabc4",
+    "url": "/React-Practice/static/js/main.6fdbb848.chunk.js"
+  },
+  {
+    "revision": "6b546ebf93ccbe47258d",
+    "url": "/React-Practice/static/js/runtime-main.c6cb60c3.js"
+  }
+]);
